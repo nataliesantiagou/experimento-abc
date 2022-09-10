a3731d4cf55dd013a43a99c43e7483e2e3b81c16
Duvan Jamid Vargas <dj.vargasc1@uniandes.edu.co>
2022-09-10 05:01:53 +0200

    descarga de la BD

--- app.py
+++ app.py
@@ -2,16 +2,17 @@ from flask import Flask
 import os
 from flask_restful import Api, Resource
 import datetime
-from .modelos import db, Healthcheck, HealthcheckSchema, Alerta
+from modelos import db, Healthcheck, HealthcheckSchema, Alerta
 from flask_cors import CORS
 import time
 import atexit
 from apscheduler.schedulers.background import BackgroundScheduler
+from flask import send_from_directory
 
 healthcheck_schema = HealthcheckSchema()
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///monitor.db'
+app = Flask(__name__, static_folder='static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/monitor.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
@@ -48,14 +49,20 @@ class VistaMonitor(Resource):
                 "id": id_dispositivo}
 
 
+class VistaDatos(Resource):
+
+    # se cambio a GET porque los heltChek son mas comunes en una peticion simple de tipo get
+    def get(self):
+        return send_from_directory(directory='static', filename='monitor.db')
+
+
 # hilo para revisar las desconexiones y generar alertas
 def revisar_desconexiones():
     print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
     with app.app_context():
-        # TODO: sumar fechas para poder hacer el filtro en la consulta
         # Consulta para filtrar dispositivosn con un tiempo mayor al pemritodo de desconexion
         current_time = datetime.datetime.utcnow()
-        one_min_ago = current_time - datetime.timedelta(minutes=1)
+        one_min_ago = current_time - datetime.timedelta(seconds=15)
 
         dispositivos = db.session.query(Healthcheck).filter(Healthcheck.desconectado.is_(False)).filter(
             Healthcheck.fecha_registro < one_min_ago).all()
@@ -81,3 +88,4 @@ atexit.register(lambda: scheduler.shutdown())
 
 api = Api(app)
 api.add_resource(VistaMonitor, '/monitor/<string:id_dispositivo>')
+api.add_resource(VistaDatos, '/datos')
